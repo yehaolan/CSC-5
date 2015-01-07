b299f1785502236d55c25dba9c7d6beeb4c43df9
benjaminyhl <yehaolan@gmail.com>
2015-01-07 09:57:19 -0800

    Haolan Ye(Benjamin)

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Haolan Ye(Benjamin)
+ * Created on January 7, 2015, 7:28 AM
+ * Purpose: set up homework
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout << "Hello World!" << endl;
+    //Exit stage right!
+    return 0;
+}
+
