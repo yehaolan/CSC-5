73bb5e0be4e76710f391b86fc631016db09957d0
benjaminyhl <yehaolan@gmail.com>
2015-01-07 11:30:18 -0800

    computer make mistake
    Haolan Ye

---
+++ Class_Lab/MathInstructorDisagreement/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Haolan Ye
+ * Created on January 7, 2015, 11:17 AM
+ * Purpose: How easy computer can make mistake
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //declare 3 variable
+    short a,b,c;
+    //Prompt for a and b
+    cout<<"Input 2 short values"<<endl;
+    cin>>a>>b;
+    //Sum the values
+    c = a + b;
+    //Output the results
+    cout << c << " = " << a << " + "<< b << endl;
+
+    return 0;
+}
+
