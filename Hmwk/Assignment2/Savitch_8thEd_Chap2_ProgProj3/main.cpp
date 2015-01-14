90e67e271f46f5aeb4d6d9712a69f5a72f3b5ec2
benjaminyhl <yehaolan@gmail.com>
2015-01-14 01:34:58 -0800

    yehaolan@gmail.com

--- Hmwk/Assignment2/Savitch_8thEd_Chap2_ProgProj3/main.cpp
+++ Hmwk/Assignment2/Savitch_8thEd_Chap2_ProgProj3/main.cpp
@@ -7,22 +7,47 @@
 
 //System Libraries
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //User Libraries
 
 //Global Constants
-
+const float PYINCS = 0.076;
 //Function Prototypes
 
 //Execution begins here
 int main(int argc, char** argv) {
     //declare variables
+    int preAnSlr; //previous annual salary
+    float rtrPay,//the amount of retroactive pay due the employee
+          nwAnSlr,//the new annual salary
+          nwMnSlr;//the new monthly salary
+    char ans;//the answer of whether run this program again
     
     
     do {
+        //Prompt user for previous annual salary
+        cout << "Your previous annual salary(dollar): " << endl;
+        cout << "Please input a positive integer." << endl;
+        cin >> preAnSlr;
         
-    } while(ans==)
+        //calculate the results
+        rtrPay = PYINCS*preAnSlr;
+        nwAnSlr = preAnSlr + rtrPay;
+        nwMnSlr = nwAnSlr/12;
+        
+        //Formatted output the results
+        cout << setprecision(2) << fixed;
+        cout << "Your retroactive pay is $" << rtrPay << endl;
+        cout << "Your new annual salary is $" << nwAnSlr << endl;
+        cout << "Your new monthly salary is $" << nwMnSlr << endl;
+        
+        //Ask if user want to repeat the program
+        cout << "Would you like to repeat this program?" << endl;
+        cout << "Type Y for yes and N for no." << endl;
+        cin >> ans;
+    } while(ans=='Y'||ans=='y'); //when answer is 'y' or 'Y',run it again
     
     //Exit stage right
     return 0;
