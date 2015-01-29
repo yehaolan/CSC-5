f560f5f8c1f1a4a78e47a820192e4498b7466f65
benjaminyhl <yehaolan@gmail.com>
2015-01-28 20:57:40 -0800

    yehaolan@gmail.com

--- Project1/Bomb_Password/main.cpp
+++ Project1/Bomb_Password/main.cpp
@@ -21,7 +21,15 @@ int main(int argc, char** argv) {
     //set seed for random number
     srand(static_cast<unsigned short>(time(0)));
     //declare variables
-    
+    const int TOTGUESS=15;//total chance of the game
+    const int SIZE=4;//the size of the char array
+    string dash;
+    string answer;
+    int gusCorr;//how many correct number have been guessed 
+    int chnslft;//chance Counter(how many many)
+    int digit;
+    char guess;
+    char pswd[SIZE];//the password store in the array
     //Exit stage right
     return 0;
 }
